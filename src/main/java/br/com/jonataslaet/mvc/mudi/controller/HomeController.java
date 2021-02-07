c20a9c916307c354f7980b199f31ca7daef381f8
Jonatas Laet <jonataslaetprogramador@gmail.com>
2021-02-06 22:45:14 -0300

    Adicionada a persistência dos dados do pedido adicionados os módulos de starter jpa e mysql

--- src/main/java/br/com/jonataslaet/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/jonataslaet/mvc/mudi/controller/HomeController.java
@@ -1,10 +1,11 @@
 package br.com.jonataslaet.mvc.mudi.controller;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,19 +15,16 @@ import br.com.jonataslaet.mvc.mudi.model.Pedido;
 
 @Controller
 public class HomeController {
+	
+	@PersistenceContext
+	private EntityManager entityManager;
 
 	@GetMapping(value="/home")
 	String retornaHello(Model model) {
-		Pedido pedido = new Pedido();
-		List<Pedido> pedidos = new ArrayList<>();
-		pedido.setNomeProduto("Relógio Amazfit GTS A1914");
-		pedido.setValorNegociado(new BigDecimal("679.3"));
-		pedido.setDataDaEntrega(LocalDate.of(2021, Month.MARCH, 5));
-		pedido.setUrlProduto("https://www.amazon.com.br/Rel%C3%B3gio-Amazfit-GTS-A1914-Preto/dp/B07XWT23FZ/");
-		pedido.setUrlImagem("https://images-na.ssl-images-amazon.com/images/I/713Wup56jcL._AC_SL1500_.jpg");
-		pedido.setDescricao("Muito mais do que um Relógio, o Amazfit é um smartwatch da Xioami com diversas funções que darão mais facilidade para sua vida, além é claro de mostrar as horas. ");
 		
-		pedidos.add(pedido);
+		List<Pedido> pedidos = new ArrayList<>();
+		Query query = entityManager.createQuery("select p from Pedido p", Pedido.class);
+		pedidos = query.getResultList();
 		
 		model.addAttribute("pedidos", pedidos);
 		return "home";
