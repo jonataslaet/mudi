10dcef00b962e7e23d8fcc678ed21b1efbc3bc8a
Jonatas Laet <jonataslaetprogramador@gmail.com>
2021-02-09 14:17:59 -0300

    Adicionada as rotas dos status do pedido pedidos listados de acordo com a aba selecionada pelo usuário

--- src/main/java/br/com/jonataslaet/mvc/mudi/PedidoRepository.java
+++ src/main/java/br/com/jonataslaet/mvc/mudi/PedidoRepository.java
@@ -1,11 +1,16 @@
 package br.com.jonataslaet.mvc.mudi;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.jonataslaet.mvc.mudi.model.Pedido;
+import br.com.jonataslaet.mvc.mudi.model.StatusPedidoEnum;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long>{
+
+	List<Pedido> findByStatus(StatusPedidoEnum status);
 	
 }
--- src/main/java/br/com/jonataslaet/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/jonataslaet/mvc/mudi/controller/HomeController.java
@@ -7,21 +7,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.jonataslaet.mvc.mudi.PedidoRepository;
 import br.com.jonataslaet.mvc.mudi.model.Pedido;
+import br.com.jonataslaet.mvc.mudi.model.StatusPedidoEnum;
 
 @Controller
+@RequestMapping(value="/home")
 public class HomeController {
 	
 	@Autowired
 	private PedidoRepository pedidoRepository;
 
-	@GetMapping(value="/home")
-	String retornaHello(Model model) {
+	@GetMapping
+	String findAll(Model model) {
 		
 		List<Pedido> pedidos = new ArrayList<>();
 		pedidos = pedidoRepository.findAll();
+		
+		model.addAttribute("pedidos", pedidos);
+		return "home";
+	}
+	
+	@GetMapping("/{status}")
+	String findAllByStatus(@PathVariable("status") String status, Model model) {
+		
+		List<Pedido> pedidos = new ArrayList<>();
+		pedidos = pedidoRepository.findByStatus(StatusPedidoEnum.valueOf(status.toUpperCase()));
 		
 		model.addAttribute("pedidos", pedidos);
 		return "home";
