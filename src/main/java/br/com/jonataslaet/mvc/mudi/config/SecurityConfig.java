1acc5d6dd0d6ffd15e5ffe2c72bd2ea001325b08
Jonatas Laet <jonataslaetprogramador@gmail.com>
2021-02-11 23:33:06 -0300

    Mudado o login para pegar do banco de dados foi utilizado o spring security com o jdbc foi criada previamente duas tabelas no banco, de acordo com a documentação do spring security

--- src/main/java/br/com/jonataslaet/mvc/mudi/config/SecurityConfig.java
+++ src/main/java/br/com/jonataslaet/mvc/mudi/config/SecurityConfig.java
@@ -1,30 +1,38 @@
 package br.com.jonataslaet.mvc.mudi.config;
 
-import org.springframework.context.annotation.Bean;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
+	@Autowired
+	private DataSource dataSource;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().anyRequest().authenticated()
-			.and().formLogin(form -> form.loginPage("/login").permitAll())
+			.and().formLogin(form -> form.loginPage("/login").defaultSuccessUrl("/home", true).permitAll())
 			.logout(logout -> logout.logoutUrl("/logout"));
 	}
 	
 	@Override
-	@Bean
-	public UserDetailsService userDetailsServiceBean() throws Exception {
-		UserDetails user = User.withDefaultPasswordEncoder().username("jonatas").password("jonatas").roles("ADM").build();
-		return new InMemoryUserDetailsManager(user);
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		
+//		UserDetails user = User.builder().username("jonatas").password(encoder.encode("jonatas")).roles("ADM").build();
+		UserDetails user = User.builder().username("maria").password(encoder.encode("jonatas")).roles("ADM").build();
+		
+		auth.jdbcAuthentication().dataSource(dataSource).passwordEncoder(encoder).withUser(user);
 	}
 }
