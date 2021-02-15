5dc3c0df28b489e565873f90cf22699d40658ac2
Jonatas Laet <jonataslaetprogramador@gmail.com>
2021-02-14 22:48:07 -0300

    Implementado o primeiro endpoint rest também foi preparada uma pasta ofertas e dentro dela uma home para em breve trabalhar com vue js

---
+++ src/main/java/br/com/jonataslaet/mvc/mudi/api/controller/PedidoRestController.java
@@ -0,0 +1,29 @@
+package br.com.jonataslaet.mvc.mudi.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.jonataslaet.mvc.mudi.model.Pedido;
+import br.com.jonataslaet.mvc.mudi.model.StatusPedidoEnum;
+import br.com.jonataslaet.mvc.mudi.repository.PedidoRepository;
+
+@RestController
+@RequestMapping("/api/pedidos")
+public class PedidoRestController {
+
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
+	@GetMapping("/aguardando")
+	public List<Pedido> getPedidosAguardandoOferta(){
+		Sort sort = Sort.by("id").descending();
+		PageRequest paginacao = PageRequest.of(0, 10, sort);
+		return pedidoRepository.findByStatus(StatusPedidoEnum.AGUARDANDO, paginacao);
+	}
+}
